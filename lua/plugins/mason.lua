9b9ea428d66abbd8877f0644c68e5ab6952f9047
Wisnu Wicaksono <wisnu.wicaksono@msiglife.co.id>
2024-04-04 23:07:05 +0700

    feat: add plugins

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -35,7 +35,6 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "python",
         "node",
         "go",
         -- add more arguments for adding more debuggers
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,8 +8,6 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
-      "javascript",
-      "typescript",
       -- add more arguments for adding more treesitter parsers
     })
   end,
